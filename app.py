d04c87d06316b421b5c8f07c0f760ef39fe2c027
harshavardhandevershetty <devershettyharshavardhan@gmail.com>
2026-06-11 12:23:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-app.py
-requirements.txt
-Dockerfile
-Jenkinsfile
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins CI/CD"
+
+app.run(host="0.0.0.0", port=5000)
